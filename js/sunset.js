fcd83874e67daa5ecd5a30e1f02eb463759fd6e3
Chris Antaki <ChrisAntaki@gmail.com>
2015-05-15 10:31:52 -0700

    Bind polyfill for Android 2.3

--- js/sunset.js
+++ js/sunset.js
@@ -82,6 +82,7 @@ function modal_show(id) {
         overlayNode.className = overlayNode.className.replace(/ ?invisible ?/, ' ');
     }, 50);
 };
+
 function modal_hide(id) {
     var overlayNode = document.getElementById(id);
     overlayNode.className += 'invisible';
@@ -336,4 +337,27 @@ function onResize() {
     for (var i = 0; i < modals.length; i++) {
         modals[i].style.maxHeight = innerHeight + 'px';
     }
+}
+
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+        if (typeof this !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function() {},
+            fBound = function() {
+                return fToBind.apply(this instanceof fNOP ? this : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
 }
