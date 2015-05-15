8ba22950a023c630791986e7d8e232d42c12b46f
Chris Antaki <ChrisAntaki@gmail.com>
2015-05-15 11:12:56 -0700

    Sorry Android 2.3

--- js/sunset.js
+++ js/sunset.js
@@ -1,4 +1,4 @@
-// Check for outdated versions of IE
+// Check for outdated browsers
 var isIE = navigator.userAgent.match(/MSIE (\d+)\./);
 if (isIE) {
     var version = +isIE[1];
@@ -7,30 +7,8 @@ if (isIE) {
     }
 }
 
-
-
-// Polyfill for Android 2.3
-if (!Function.prototype.bind) {
-    Function.prototype.bind = function(oThis) {
-        if (typeof this !== 'function') {
-            // closest thing possible to the ECMAScript 5
-            // internal IsCallable function
-            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
-        }
-
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function() {},
-            fBound = function() {
-                return fToBind.apply(this instanceof fNOP ? this : oThis,
-                    aArgs.concat(Array.prototype.slice.call(arguments)));
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-    };
+if (navigator.userAgent.match(/Android 2\.3/)) {
+    alert('Unfortunately your browser, Android 2.3, is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
 }
 
 
